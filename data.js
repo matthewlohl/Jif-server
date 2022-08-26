d916e4eb6d148df6c9bc79fc5cf10d8fd6c18d83
Matthew Lo <matthewlol316@gmail.com>
2022-08-26 11:43:13 +0100

    create data

---
+++ data.js
@@ -0,0 +1,41 @@
+const data = [
+    {
+        id : 1,
+        title: "Pac Man",
+        date: "03122021",
+        text: "Animated gifs and lores images from the classic 1980 Namco arcade game Pac-Man. Fun images to share with friends, and userful resources for web designers. To save these images, right click and select SAVE AS. Please be patient and allow the page to load (there are alot of images on this page).",
+        gif: "https://i.imgur.com/a3u5DxA.gif",
+        emoji: ['&#128077;','&#128078;','&#129505;'],
+        comment: ['This is a very great post', 'I love it', 'very boring', 'My favourite gif!!!!']
+    },
+
+    {
+        id : 2,
+        title: "Surprised White Guy",
+        date: "12162017",
+        text: "It originated in 2013, when Blinking White Guy was just Drew Scanlon, a video editor at gaming site giantbomb.com. Wait...the blinking white guy is an ACTUAL GUY? He regularly appeared on video podcast show Unprofessional Fridays, as he did on that fateful day in 2013.",
+        gif: "https://miro.medium.com/max/390/1*eKABkUzJKEc9oWoFf9qrpQ.gif",
+        emoji: ['&#128077;','&#128078;','&#129505;'],
+        comment: ['Classic!!', 'LMAO', 'I use it everyday']
+    },
+
+    {
+        id : 3,
+        title: "Leonardo Cheers",
+        date: "12162017",
+        text: "With drink in hand, Leonardo DiCaprio’s been cheers-ing for practically his entire acting career. Many on the Internet quickly realized this after he was so opulently introduced in this year’s The Great Gatsby and our collective minds flashed back to 1997’s Titanic. That’s an easy comparison to make, but did you know he also raises his glass in 2002’s Gangs of New York? And in 1998’s The Man in the Iron Mask? We’ve put together a GIF timeline of Leo’s toasts (some more direct than others), so fill up your own glass and raise it in his honor. (Though you’ll never be as good at it as he is)'Come, bitter conduct, come, unsavory guide. Thou desperate pilot, now at once run on the dashing rocks thy seasick, weary bark. Here’s to my love!” —1996’s Romeo + Juliet:'",
+        gif: "https://c.tenor.com/j9RiUvoQK5YAAAAM/cheers-leonardo-dicaprio.gif",
+        emoji: ['&#128077;','&#128078;','&#129505;'],
+        comment: ['I love Leonardo', 'HAHAHHAA', 'muahhh', 'Love it']
+    },
+
+    {
+        id : 4,
+        title: "Who is Gavin?",
+        date: "02262017",
+        text: "Vine-famous Gavin Thomas with is uncle, Nick Mastodon, on Sunday, Feb. 26, 2017 at his grandmother's home in Eden Prairie. Gavin became famous for his reaction faces. You might know him as the meme kid, the boy with the lizard or the six-year-old from Vine.",
+        gif: "https://c.tenor.com/7AemUFVfkkIAAAAC/bitch-what.gif",
+        emoji: ['&#128077;','&#128078;','&#129505;', '&#129505;'],
+        comment: ["Oh he's called Gavin?", 'What?!?', 'muahhh', 'Love it']
+    }
+]
