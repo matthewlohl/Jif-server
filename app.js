3c0d2fb2082a59482ec36964a9f2ee04412ba76a
Littlerou <izzymelvin4@gmail.com>
2022-08-26 11:49:21 +0100

    add index.js and app.js files

---
+++ app.js
@@ -0,0 +1,40 @@
+const express = require('express')
+const app = express()
+const cors = require('cors');
+app.use(cors());
+app.use(express.json());
+
+
+// GET data
+
+
+
+
+
+// GET individual Id
+
+
+
+
+
+// Delete ID 
+
+
+
+
+
+// GET new 
+
+
+// POST 
+
+
+
+// GET random
+
+
+
+
+// GET edit 
+
+
