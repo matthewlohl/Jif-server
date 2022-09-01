81167624216b4be8f8c00590503af34ae01bef5e
Littlerou <izzymelvin4@gmail.com>
2022-09-01 15:26:53 +0100

    shorten data

--- data.js
+++ data.js
@@ -23,7 +23,7 @@ const data = [
         id : 3,
         title: "Leonardo Cheers",
         date: "12162017",
-        text: "With drink in hand, Leonardo DiCaprio’s been cheers-ing for practically his entire acting career. Many on the Internet quickly realized this after he was so opulently introduced in this year’s The Great Gatsby and our collective minds flashed back to 1997’s Titanic. That’s an easy comparison to make, but did you know he also raises his glass in 2002’s Gangs of New York? And in 1998’s The Man in the Iron Mask? We’ve put together a GIF timeline of Leo’s toasts (some more direct than others), so fill up your own glass and raise it in his honor. (Though you’ll never be as good at it as he is)'Come, bitter conduct, come, unsavory guide. Thou desperate pilot, now at once run on the dashing rocks thy seasick, weary bark. Here’s to my love!” —1996’s Romeo + Juliet:'",
+        text: "With drink in hand, Leonardo DiCaprio’s been ",
         gif: "https://c.tenor.com/j9RiUvoQK5YAAAAM/cheers-leonardo-dicaprio.gif",
         emoji: ['&#128077;','&#128078;','&#129505;'],
         comment: ['I love Leonardo', 'HAHAHHAA', 'muahhh', 'Love it']
